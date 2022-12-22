f9840ae8d231e710448420147c0f42b87fa673b6
cesarmsj <cesarsj@unipam.edu.br>
2022-12-22 16:04:03 -0300

    Fiz diversos ajustes no component slider

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -2,22 +2,19 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import Banner from "../../assets/images/banner.svg";
-import BannerMobile from "../../assets/images/banner-mobile.svg";
 
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-import { Container, Image } from "./styles";
+import { Image } from "./styles";
 
 import { Pagination, Navigation } from "swiper";
 
-export default function App() {
+export default function Slider() {
 
   return (
-    <Container>
       <Swiper
-      className="mySwiper"
       slidesPerView={"auto"}
       centeredSlides={true}
       spaceBetween={30}
@@ -47,31 +44,5 @@ export default function App() {
           </SwiperSlide>
         
       </Swiper>
-
-    {/* mobile */}
-
-      <Swiper
-        style={{
-        '--swiper-navigation-color': '#FFF',
-        '--swiper-navigation-size': '30px',
-        '--swiper-pagination-color': '#FFF',
-        '--swiper-pagination-bullet-inactive-color': '#FFF',
-        '--swiper-pagination-bullet-inactive-opacity': '0.4',
-        }}
-        loop={true}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        className="mySwiper mobile"
-        centeredSlides={true}
-      >
-          <SwiperSlide><img src={BannerMobile} alt="Banner" /></SwiperSlide>
-          <SwiperSlide><img src={BannerMobile} alt="Banner" /></SwiperSlide>
-          <SwiperSlide><img src={BannerMobile} alt="Banner" /></SwiperSlide>
-        
-      </Swiper>
-    </Container>
   );
 }
--- src/components/Slider/styles.js
+++ src/components/Slider/styles.js
@@ -1,32 +1,11 @@
 import styled from 'styled-components';
 
-const Container = styled.div` 
-.mySwiper {
-  width: 100%;
-  height: 40.875rem;
-}
-
-.mobile {
-  display: none;
-}
-
-@media (max-width: 768px) {
-    .desktop {
-      display: none;
-    }
-
-    .mobile {
-      display: block;
-    }
-}
-`
-
 const Image = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
 `;
 
-export { Container, Image };
+export { Image };
 
 
