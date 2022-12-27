ab0b4a3591be8b7d18b177b9ff722119dc168bb3
cesarmsj <cesarvn2014@gmail.com>
2022-12-27 13:43:08 -0300

    Ajustei o footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 export const Container = styled.div`
   background-color:${({theme}) => theme.colors.button};
   display:flex;
+  align-items: center;
+  justify-content: space-between;
   width: 100%;
   height: 6.375rem;
   padding: 3.75rem 8rem;
