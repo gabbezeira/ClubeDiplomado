047d9fe599227731c079bb3563c6981ab8946e13
cesarmsj <cesarsj@unipam.edu.br>
2022-12-28 09:53:36 -0300

    Ajustei margem do titulo de inscreva-se

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -2,8 +2,8 @@ import { Container } from "./styles";
 
 export default function Formulario() {
   return (
-    <Container id="formulario" style={{marginTop: '5rem'}}>
-      <div className="header">
+    <Container id="formulario" >
+      <div className="header" style={{marginTop: '5rem'}}>
         <h2>Inscreva-se</h2>
         <div className="underline"></div>
       </div>
