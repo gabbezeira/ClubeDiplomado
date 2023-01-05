7bd7bca155aaff2863d26f763c20723d2fc2198e
Gabriel  Alves <68339733+GabrielWeises@users.noreply.github.com>
2023-01-05 12:01:01 -0300

    removi o style.css

--- src/components/Conveniados/index.js
+++ src/components/Conveniados/index.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 import SearchSVG from "../../assets/images/Icons/search.png";
 import { Container } from "./styles";
 import { buscaParceiros } from "../../services/requests/parceiros";
-import { PhoneIcon, MapPinIcon } from '@heroicons/react/24/solid'
-import {isMobile} from 'react-device-detect';
-import Modal from 'react-modal';
+import { PhoneIcon, MapPinIcon } from "@heroicons/react/24/solid";
+import { isMobile } from "react-device-detect";
+import Modal from "react-modal";
 
 export default function Conveniados() {
   const [parceiros, setParceiros] = useState([]);
@@ -61,95 +61,103 @@ export default function Conveniados() {
       {parceiros.length >= 1 ? (
         <div className="overflow">
           <div className="table">
-            {parceiros.map((parceiro) => (
-              isMobile ?
-              <div className="tr" key={parceiro.IdPessoa} onClick={openModal}>
-                <div className="td">
-                  <b>
-                    {parceiro.PessoaJuridica?.NomeFantasia
-                      ? ajustaNome(parceiro.PessoaJuridica?.NomeFantasia)
-                      : ajustaNome(parceiro.Nome)}
-                  </b>
+            {parceiros.map((parceiro) =>
+              isMobile ? (
+                <div className="tr" key={parceiro.IdPessoa} onClick={openModal}>
+                  <div className="td">
+                    <b>
+                      {parceiro.PessoaJuridica?.NomeFantasia
+                        ? ajustaNome(parceiro.PessoaJuridica?.NomeFantasia)
+                        : ajustaNome(parceiro.Nome)}
+                    </b>
+                  </div>
+                  <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    // style={customStyles}
+                    contentLabel="Example Modal"
+                  >
+                    <h2>Hello</h2>
+                    <button onClick={closeModal}>close</button>
+                    <div>I am a modal</div>
+                  </Modal>
                 </div>
-                <Modal
-                  isOpen={modalIsOpen}
-                  onRequestClose={closeModal}
-                  // style={customStyles}
-                  contentLabel="Example Modal"
-                >
-                  <h2>Hello</h2>
-                  <button onClick={closeModal}>close</button>
-                  <div>I am a modal</div>
-                </Modal>
-              </div>
-               :
-              <div className="tr" key={parceiro.IdPessoa}>
-
-                <div className="td">
-                  <b>
-                    {parceiro.PessoaJuridica?.NomeFantasia
-                      ? ajustaNome(parceiro.PessoaJuridica?.NomeFantasia)
-                      : ajustaNome(parceiro.Nome)}
-                  </b>
-                </div>
-                <div className="td" id="hide">
-                  {parceiro.Contratos.map((c) => {
-                    return (
-                      <div key={c.IdContrato}>
-                        {c.Observacao && (
-                          <p>
-                            {c.Observacao} <br />
-                            <br />
-                          </p>
-                        )}
-                        {c.Beneficios.map((b) => {
-                          return (
-                            <p key={b.IdBeneficio}>
-                              <b>Benefício: </b>
-                              {b.PercDesconto > 0
-                                ? b.PercDesconto + "%"
-                                : b.ValorDesconto + " reais"}{" "}
-                              {b.Observacao}
-                              <b className="b-categoria">Categoria:</b>{" "}
-                              {b.BeneficioCategoria}
+              ) : (
+                <div className="tr" key={parceiro.IdPessoa}>
+                  <div className="td">
+                    <b>
+                      {parceiro.PessoaJuridica?.NomeFantasia
+                        ? ajustaNome(parceiro.PessoaJuridica?.NomeFantasia)
+                        : ajustaNome(parceiro.Nome)}
+                    </b>
+                  </div>
+                  <div className="td" id="hide">
+                    {parceiro.Contratos.map((c) => {
+                      return (
+                        <div key={c.IdContrato}>
+                          {c.Observacao && (
+                            <p>
+                              {c.Observacao} <br />
+                              <br />
                             </p>
-                          );
-                        })}
-                      </div>
-                    );
-                  })}
-                </div>
-                <div className="td" id="hide" style={{justifyContent: "space-between"}}>
-                  <div style={{width: '80%', marginRight: '1rem'}}>
-                    {parceiro.Endereco?.TipoLogradouro +
-                      " " +
-                      parceiro.Endereco?.Logradouro +
-                      " " +
-                      parceiro.Endereco?.Numero +
-                      ", " +
-                      parceiro.Endereco?.Complemento +
-                      " " +
-                      parceiro.Endereco?.Bairro +
-                      " " +
-                      parceiro.Endereco?.Cidade +
-                      ", " +
-                      parceiro.Endereco?.UF}
+                          )}
+                          {c.Beneficios.map((b) => {
+                            return (
+                              <p key={b.IdBeneficio}>
+                                <b>Benefício: </b>
+                                {b.PercDesconto > 0
+                                  ? b.PercDesconto + "%"
+                                  : b.ValorDesconto + " reais"}{" "}
+                                {b.Observacao}
+                                <b className="b-categoria">Categoria:</b>{" "}
+                                {b.BeneficioCategoria}
+                              </p>
+                            );
+                          })}
+                        </div>
+                      );
+                    })}
                   </div>
-                  <div>
-                    <a href="tel:34996937841" >
-                      <PhoneIcon width={20} height={20} color={'#034870'} />
-                    </a>
-                    <a href={linkEndereco(parceiro.Endereco?.Latitude, parceiro.Endereco?.Longitude)} 
-                      target='_blank'
-                      rel="noreferrer" 
-                      style={{marginLeft: "0.5rem"}}>
-                      <MapPinIcon width={20} height={20} color={'#034870'} />
-                    </a>
+                  <div
+                    className="td"
+                    id="hide"
+                    style={{ justifyContent: "space-between" }}
+                  >
+                    <div style={{ width: "80%", marginRight: "1rem" }}>
+                      {parceiro.Endereco?.TipoLogradouro +
+                        " " +
+                        parceiro.Endereco?.Logradouro +
+                        " " +
+                        parceiro.Endereco?.Numero +
+                        ", " +
+                        parceiro.Endereco?.Complemento +
+                        " " +
+                        parceiro.Endereco?.Bairro +
+                        " " +
+                        parceiro.Endereco?.Cidade +
+                        ", " +
+                        parceiro.Endereco?.UF}
+                    </div>
+                    <div>
+                      <a href="tel:34996937841">
+                        <PhoneIcon width={20} height={20} color={"#034870"} />
+                      </a>
+                      <a
+                        href={linkEndereco(
+                          parceiro.Endereco?.Latitude,
+                          parceiro.Endereco?.Longitude
+                        )}
+                        target="_blank"
+                        rel="noreferrer"
+                        style={{ marginLeft: "0.5rem" }}
+                      >
+                        <MapPinIcon width={20} height={20} color={"#034870"} />
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
-             
-            ))}
+              )
+            )}
           </div>
         </div>
       ) : (
